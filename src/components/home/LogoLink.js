98cb6d042ccdf082c8ffd9ffb4f2eb2f3b458ff4
Isikalu Tobi <davisleevtobi@gmail.com>
2024-08-31 15:47:06 +0100

    fix logo container resizing issue

--- src/components/home/LogoLink.js
+++ src/components/home/LogoLink.js
@@ -6,14 +6,16 @@ export default function LogoLink() {
   return (
     <div className="mx-5 sm:px-10">
       <Link href="/">
-        <div class='relative h-20 w-40'>
-        <Image
+        <div class='relative h-20 w-40 bg-appgray bg-opacity-10'>
+          <div className="absolute inset-0 bg-appgray bg-opacity-10">
+           <Image
             src="/LaCiudadcropped.webp"
             alt="Your Brand Logo"
             layout="fill"
             objectFit="contain" 
             objectPosition="center"
-        />
+           />
+          </div>
         </div>
       </Link>
     </div>
