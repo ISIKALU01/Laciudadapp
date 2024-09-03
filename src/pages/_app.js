568d689e93471ebc4bec04a7da50189c34f2c19a
Isikalu Tobi <davisleevtobi@gmail.com>
2024-09-03 09:50:26 +0100

    fix bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ export default function App({ Component, pageProps }) {
     router.events.on('routeChangeError', handleComplete);
 
     const checkImagesLoaded = () => {
-      const images = document.querySelectorAll('.logo');
+      const images = document.querySelectorAll('');
       console.log(images)
 
       if (images.length === 0) {
